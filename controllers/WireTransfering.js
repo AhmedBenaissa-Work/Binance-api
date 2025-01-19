fed765f92348dac44e86017f41d43dcb7545adb4
ahmedbnissa <abenaissa@welyne.com>
2025-01-19 15:43:29 +0100

    funds transfer api

--- controllers/WireTransfering.js
+++ controllers/WireTransfering.js
@@ -6,7 +6,8 @@ const url = process.env.Infura_network;
 const {Web3} = require('web3');
 const web3 = new Web3(url);
 const path = require('path');
-const wallet = require('../Models/wallet');
+const wallets = require('../Models/wallet');
+const accounts = require('../Models/account');
 const artifactPath = path.join(__dirname, '../Contracts/Sandbox/artifacts/WalletAccess.json');
 const contractArtifact = JSON.parse(fs.readFileSync(artifactPath, 'utf8'));
 
@@ -16,7 +17,7 @@ const contractAddress = process.env.sandbox_contract_address;
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 const withdraw =  async (req,res)=>{
     const auth_token = req.headers.authorization
-    token_data=jwt.decode(authToken,'')
+    token_data=jwt.decode(auth_token,'')
     if(auth_token==undefined)
     {
         res.status(401).send("un-authorized")
@@ -40,6 +41,32 @@ const withdraw =  async (req,res)=>{
   }
     }
 }
+const exchange = async(req,res)=>{
+    const auth_token = req.headers.authorization
+    if(auth_token==undefined){
+        res.status(401).send("forbidden")
+    }
+    else{
+    token_data=jwt.decode(auth_token,'')
+    
+    const sender = token_data.address
+    const receiver=req.body.receiver
+    const amount=req.body.amount
+    
+    try{
+
+    
+    const doc = await wallets.find({address:sender});
+    const doc_0 = await wallets.find({address:receiver});
+    sender_wallet=await wallets.findById({address:sender}).updateOne({Balance:doc[0].Balance-amount})
+    receiver_wallet=await wallets.findById({address:receiver}).updateOne({Balance:doc_0[0].Balance+amount})
+    res.send("Funds Transferred")
+    }catch(error){
+        res.status(401).send(error)
+    }
+}
+
+}
 module.exports = {
-    withdraw
+    withdraw,exchange
 }
